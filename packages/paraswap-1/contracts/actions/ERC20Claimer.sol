3ebf69e5dae063cb2cc2bb56fd16484c3043f7fe
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2022-10-31 09:28:36 -0300

    Paraswap: Add slippage to signature (#8)

--- packages/paraswap-1/contracts/actions/ERC20Claimer.sol
+++ packages/paraswap-1/contracts/actions/ERC20Claimer.sol
@@ -16,17 +16,22 @@ pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
 
+import '@mimic-fi/v2-helpers/contracts/math/FixedPoint.sol';
 import '@mimic-fi/v2-swap-connector/contracts/ISwapConnector.sol';
 
 import './BaseClaimer.sol';
 
 contract ERC20Claimer is BaseClaimer {
+    using FixedPoint for uint256;
+
     // Base gas amount charged to cover gas payment
     uint256 public constant override BASE_GAS = 22e3;
 
     address public swapSigner;
+    uint256 public maxSlippage;
 
     event SwapSignerSet(address swapSigner);
+    event MaxSlippageSet(uint256 maxSlippage);
 
     constructor(address admin, address registry) BaseClaimer(admin, registry) {
         // solhint-disable-previous-line no-empty-blocks
@@ -37,32 +42,49 @@ contract ERC20Claimer is BaseClaimer {
         emit SwapSignerSet(newSwapSigner);
     }
 
+    function setMaxSlippage(uint256 newMaxSlippage) external auth {
+        require(newMaxSlippage <= FixedPoint.ONE, 'CLAIMER_SLIPPAGE_ABOVE_ONE');
+        maxSlippage = newMaxSlippage;
+        emit MaxSlippageSet(newMaxSlippage);
+    }
+
     function call(
         address tokenIn,
         uint256 amountIn,
         uint256 minAmountOut,
+        uint256 expectedAmountOut,
         uint256 deadline,
         bytes memory data,
         bytes memory sig
     ) external auth {
-        (isRelayer[msg.sender] ? _relayedCall : _call)(tokenIn, amountIn, minAmountOut, deadline, data, sig);
+        (isRelayer[msg.sender] ? _relayedCall : _call)(
+            tokenIn,
+            amountIn,
+            minAmountOut,
+            expectedAmountOut,
+            deadline,
+            data,
+            sig
+        );
     }
 
     function _relayedCall(
         address tokenIn,
         uint256 amountIn,
         uint256 minAmountOut,
+        uint256 expectedAmountOut,
         uint256 deadline,
         bytes memory data,
         bytes memory sig
     ) internal redeemGas {
-        _call(tokenIn, amountIn, minAmountOut, deadline, data, sig);
+        _call(tokenIn, amountIn, minAmountOut, expectedAmountOut, deadline, data, sig);
     }
 
     function _call(
         address tokenIn,
         uint256 amountIn,
         uint256 minAmountOut,
+        uint256 expectedAmountOut,
         uint256 deadline,
         bytes memory data,
         bytes memory sig
@@ -70,7 +92,8 @@ contract ERC20Claimer is BaseClaimer {
         address wrappedNativeToken = wallet.wrappedNativeToken();
         require(tokenIn != wrappedNativeToken && tokenIn != Denominations.NATIVE_TOKEN, 'ERC20_CLAIMER_INVALID_TOKEN');
         _validateThreshold(wrappedNativeToken, minAmountOut);
-        _validateSwapSignature(tokenIn, wrappedNativeToken, amountIn, minAmountOut, deadline, data, sig);
+        _validateSlippage(minAmountOut, expectedAmountOut);
+        _validateSignature(tokenIn, wrappedNativeToken, amountIn, minAmountOut, expectedAmountOut, deadline, data, sig);
 
         bytes memory claim = abi.encodeWithSelector(IFeeClaimer.withdrawSomeERC20.selector, tokenIn, amountIn, wallet);
         _claim(claim);
@@ -86,19 +109,41 @@ contract ERC20Claimer is BaseClaimer {
         emit Executed();
     }
 
-    function _validateSwapSignature(
+    function _validateSlippage(uint256 minAmountOut, uint256 expectedAmountOut) internal view {
+        require(minAmountOut <= expectedAmountOut, 'MIN_AMOUNT_GT_EXPECTED_AMOUNT');
+        uint256 slippage = FixedPoint.ONE - minAmountOut.divUp(expectedAmountOut);
+        require(slippage <= maxSlippage, 'CLAIMER_SLIPPAGE_TOO_BIG');
+    }
+
+    function _validateSignature(
         address tokenIn,
         address tokenOut,
         uint256 amountIn,
         uint256 minAmountOut,
+        uint256 expectedAmountOut,
         uint256 deadline,
         bytes memory data,
         bytes memory sig
     ) internal view {
-        bool isBuy = false;
-        bytes32 hash = keccak256(abi.encodePacked(tokenIn, tokenOut, isBuy, amountIn, minAmountOut, deadline, data));
-        address signer = ECDSA.recover(ECDSA.toEthSignedMessageHash(hash), sig);
+        bytes32 message = _hash(tokenIn, tokenOut, amountIn, minAmountOut, expectedAmountOut, deadline, data);
+        address signer = ECDSA.recover(ECDSA.toEthSignedMessageHash(message), sig);
         require(signer == swapSigner, 'INVALID_SWAP_SIGNATURE');
         require(block.timestamp <= deadline, 'SWAP_DEADLINE_EXPIRED');
+    }
+
+    function _hash(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn,
+        uint256 minAmountOut,
+        uint256 expectedAmountOut,
+        uint256 deadline,
+        bytes memory data
+    ) private pure returns (bytes32) {
+        bool isBuy = false;
+        return
+            keccak256(
+                abi.encodePacked(tokenIn, tokenOut, isBuy, amountIn, minAmountOut, expectedAmountOut, deadline, data)
+            );
     }
 }
