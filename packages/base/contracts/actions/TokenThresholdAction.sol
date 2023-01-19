6ded73bc9e67304ebebbf043bcb893b7fad8f910
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2023-01-19 17:29:51 -0300

    Base: Update @mimic/v2-helpers to v0.1.1 (#52)

--- packages/base/contracts/actions/TokenThresholdAction.sol
+++ packages/base/contracts/actions/TokenThresholdAction.sol
@@ -48,8 +48,7 @@ abstract contract TokenThresholdAction is BaseAction {
      * @param amount Amount of tokens to validate the threshold
      */
     function _passesThreshold(address token, uint256 amount) internal view returns (bool) {
-        address queryToken = Denominations.isNativeToken(token) ? smartVault.wrappedNativeToken() : token;
-        uint256 price = smartVault.getPrice(queryToken, thresholdToken);
+        uint256 price = smartVault.getPrice(_wrappedIfNative(token), thresholdToken);
         // Result balance is rounded down to make sure we always match at least the threshold
         return amount.mulDown(price) >= thresholdAmount;
     }
